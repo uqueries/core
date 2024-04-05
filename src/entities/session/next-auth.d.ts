f894832aa50fdab3263406a39ae454949555e6b8
ClausCode <nclaus.code@gmail.com>
2024-04-05 23:05:35 +0300

    feat(base-auth): modify next-auth types

---
+++ src/entities/session/next-auth.d.ts
@@ -0,0 +1,16 @@
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string;
+      email: string;
+      image?: string;
+    };
+  }
+
+  interface User {
+    id: string;
+    name?: string;
+    email: string;
+    image?: string;
+  }
+}
