694961baa8a754b360d93ea4ec0ed4953cae87a3
ClausCode <nclaus.code@gmail.com>
2024-04-05 22:59:49 +0300

    chor(app): add env private/public config

--- src/entities/session/next-auth.config.ts
+++ src/entities/session/next-auth.config.ts
@@ -1,11 +1,18 @@
 import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { dbClient } from "@/shared/lib/db";
+import { privateConfig } from "@/shared/config/private";
+import { compact } from "lodash-es";
 
 export const nextAuthConfig: AuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_APP_ID ?? "",
-      clientSecret: process.env.GITHUB_APP_SECRET ?? "",
-    }),
-  ],
+  adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
+  providers: compact([
+    privateConfig.GITHUB_APP_ID &&
+      privateConfig.GITHUB_APP_SECRET &&
+      GithubProvider({
+        clientId: privateConfig.GITHUB_APP_ID,
+        clientSecret: privateConfig.GITHUB_APP_SECRET,
+      }),
+  ]),
 };
