69556e4a999543d7999dd530035b3bfae1cb8a52
ClausCode <nclaus.code@gmail.com>
2024-04-10 21:15:59 +0300

    test(app): add e2e auth tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ require("dotenv").config();
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  //fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -32,14 +32,11 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    { name: "setup", testMatch: /.*\.setup\.ts/ },
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      dependencies: ["setup"],
     },
 
     /* Test against mobile viewports. */
--- src/entities/user/_domain/ability.ts
+++ src/entities/user/_domain/ability.ts
@@ -2,10 +2,14 @@ import { ROLES, SessionEntity, UserId } from "@/entities/user/_domain/types";
 
 export const createUserAbility = (session: SessionEntity) => ({
   canGetUser: (userId: UserId) =>
-    session.user.id === userId || session.user.role === ROLES.SUPER,
+    session.user.id === userId ||
+    session.user.role === ROLES.SUPER ||
+    session.user.role === ROLES.ADMIN,
 });
 
 export const createProfileAbility = (session: SessionEntity) => ({
   canUpdateProfile: (userId: UserId) =>
-    session.user.id === userId || session.user.role === ROLES.SUPER,
+    session.user.id === userId ||
+    session.user.role === ROLES.SUPER ||
+    session.user.role === ROLES.ADMIN,
 });
